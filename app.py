f9815b5417e9ae8a5c6bd53d06f1cb3c92608917
Bev Edwards-Smith <bedwardssmith@yahoo.ca>
2021-05-26 18:52:21 -0400

    updated files

--- app.py
+++ app.py
@@ -2,48 +2,27 @@
 import os
 import numpy as np
 import pickle
-from flask import Flask, request, redirect, url_for, render_template 
-#from __future__ import unicode_literals
-from nltk.corpus import stopwords
-from nltk.tokenize import WordPunctTokenizer
-from string import punctuation
-from nltk.stem import WordNetLemmatizer
-import nltk
-nltk.download('stopwords')
-nltk.download('wordnet')
-nltk.download('punkt')
-nltk.download('averaged_perceptron_tagger')
-from sklearn.feature_extraction.text import CountVectorizer
-#from sklearn.externals import joblib
-from sklearn.naive_bayes import MultinomialNB
+from flask import Flask, request, json, jsonify, request
+from flask.templating import render_template
 
 
-os.chdir('/Users/16472/Bootcamp/local_version')
-filename = 'test1_model.pkl'
-load_model = pickle.load(open(filename, 'rb'))
+#os.chdir('/Users/16472/Bootcamp/local_version')
+#filename = 'test1_model.pkl'
+#load_model = pickle.load(open(filename, 'rb'))
 
 # initialize the flask app
 app = Flask(__name__)
 
-cv = CountVectorizer()
-clf = MultinomialNB()
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/submit',methods=["POST"])
-def submit():
-    pickle.dump(clf, 'test1_model.pkl')
-    clf = pickle.load(load_model)
+@app.route('/api/submit',methods=["POST"])
+def predict():
+    content = request.json['userInput']
+    return jsonify(content)
 
-    if request.method == 'POST':
-        message = request.form['text']
-        data = [message]
-        vect = cv.transform(data).toarray()
-        my_prediction1 = clf.predict(vect)
-
-    return render_template('index.html', predictions_class1 = my_prediction1)
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ static/js/app.js
@@ -0,0 +1,32 @@
+
+var alertDiv = document.getElementById("alert-row");
+var textArea = document.getElementById("user-input");
+
+function submitUserInformation() {
+    var userText = textArea.value.replace(/\s+/g, ' ').split(' ').filter((e) => e.length > 0);;
+    console.log(userText);
+
+
+    fetch('http://index/submit', {
+        method: 'POST',
+        body:JSON.stringify({
+            userInput: userText
+        }),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8'
+        }
+    }).then(response => {
+        return response.text();
+    }).then(data => {
+        console.log(data)
+        alertDiv.classList = "row-alert alert-success";
+        alertDiv.innerHTML = '<h3>Sent Successfully</h3><br><p>${data}</p>'
+    }).catch ( err => {
+        alertDiv.innerHTML = "Error: ${err}"
+    });
+}
+
+var submitBtn = document.getElementById("submit-this");
+submitBtn.addEventListener("click", submitUserInformation)
+
+
