42c4e71dec77187f790b7d1cbdfbcf0a71df086b
Bev Edwards-Smith <bedwardssmith@yahoo.ca>
2021-05-27 14:46:53 -0400

    html corrected for bad syntax

--- static/js/app.js
+++ static/js/app.js
@@ -2,10 +2,12 @@
 var alertDiv = document.getElementById("alert-row");
 var textArea = document.getElementById("user-input");
 
+
+
 function submitUserInformation() {
     var userText = textArea.value.replace(/\s+/g, ' ').split(' ').filter((e) => e.length > 0);;
     console.log(userText);
-
+    console.log("check")
 
     fetch('http://index/submit', {
         method: 'POST',
@@ -19,14 +21,17 @@ function submitUserInformation() {
         return response.text();
     }).then(data => {
         console.log(data)
-        alertDiv.classList = "row-alert alert-success";
+        alertDiv.classList = "alert-row alert-success";
         alertDiv.innerHTML = '<h3>Sent Successfully</h3><br><p>${data}</p>'
     }).catch ( err => {
         alertDiv.innerHTML = "Error: ${err}"
     });
 }
 
+
+
+
 var submitBtn = document.getElementById("submit-this");
-submitBtn.addEventListener("click", submitUserInformation)
+submitBtn.addEventListener("click", submitUserInformation);
 
 
