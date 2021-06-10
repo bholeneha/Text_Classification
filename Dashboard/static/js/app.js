681447d815d0c0349903f3e8d468ebd03dcd5093
Neha Bhole <nehasbhole@gmail.com>
2021-06-10 10:12:05 -0400

    Rearranging files and folder

---
+++ Dashboard/static/js/app.js
@@ -0,0 +1,37 @@
+var reset = document.getElementById("reset");
+
+var alertDiv = document.getElementById("alert-row");
+var textArea = document.getElementById("user-input");
+
+
+function submitUserInformation() {
+    var userText = textArea.value.replace(/[^A-Za-z]+/g, ' ');
+    console.log(userText);
+    fetch('/api/submit', {
+        method: 'POST',
+        body:JSON.stringify({
+            userInput: userText
+        }),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8'
+        }
+    }).then(response => {
+        return response.text();
+    }).then(data => {
+        console.log(typeof data)
+        var classification = data.replace(/[^A-Za-z]+/g, ' ');
+        alertDiv.classList = "row-alert alert-success";
+        alertDiv.innerHTML = `<p class="text-dark">${classification}</p>`
+    }).catch ( err => {
+        alertDiv.innerHTML = `Error: ${err}` 
+    });
+}
+
+function clearText() {
+    console.log("checking")
+    textArea.value = ""
+    alertDiv.innerHTML = ""
+}
+var submitBtn = document.getElementById("submit-this");
+submitBtn.addEventListener("click", submitUserInformation)
+reset.addEventListener("click", clearText)
